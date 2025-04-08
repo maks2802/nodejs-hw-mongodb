e6364a7193ad5327ab239615d17bea8b6067b049
maks2802 <komrakovmax2004@gmail.com>
2025-04-08 18:01:34 +0300

    final commit

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,6 +1,7 @@
 import nodemailer from 'nodemailer';
 import { SMTP } from '../constants/index.js';
 import { getEnvVar } from './getEnvVar.js';
+import createHttpError from 'http-errors';
 
 const transporter = nodemailer.createTransport({
   host: getEnvVar(SMTP.SMTP_HOST),
@@ -12,5 +13,12 @@ const transporter = nodemailer.createTransport({
 });
 
 export const sendMail = async (options) => {
-  return await transporter.sendMail(options);
+  try {
+    await transporter.sendMail(options);
+  } catch (err) {
+    throw createHttpError(
+      500,
+      'Failed to send the email, please try again later.',
+    );
+  }
 };
