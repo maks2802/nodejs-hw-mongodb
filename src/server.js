01687f10c385dc5b30abc78dd73c17e3ebe22587
maks2802 <komrakovmax2004@gmail.com>
2025-04-11 22:14:06 +0300

    swagger commit

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { getEnvVar } from './utils/getEnvVar.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -18,6 +19,7 @@ export const setupServer = () => {
   app.use(cookieParser());
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(
     pino({
